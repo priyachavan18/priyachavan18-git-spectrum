cd547ec046afb804db82b96505e023f3fad536a3
PRIYA CHAVAN <priyachavan0218@gmail.com>
2026-07-05 00:13:35 +0530

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -1,8 +1,11 @@
-from components.insights import show_insights
 import streamlit as st
-from components.filters import sidebar_filters
+
 from utils.load_data import load_data
+
+from components.filters import sidebar_filters
 from components.kpi_cards import show_kpis
+from components.insights import show_insights
+
 from components.charts import (
     prepare_data,
     monthly_revenue_chart,
@@ -15,104 +18,103 @@ from components.charts import (
     daily_sales_chart,
 )
 
+# ==========================================
+# LOAD DATA
+# ==========================================
 
-# --------------------------------------------------
-# PAGE CONFIG
-# --------------------------------------------------
+df = load_data()
+df = prepare_data(df)
+df = sidebar_filters(df)
 
-
-# --------------------------------------------------
-# TITLE
-# --------------------------------------------------
+# ==========================================
+# PAGE TITLE
+# ==========================================
 
 st.title("📊 Executive Dashboard")
 st.caption("Real-Time E-Commerce Analytics")
 
 st.divider()
 
-# --------------------------------------------------
-# LOAD DATA
-# --------------------------------------------------
-
-df = load_data()
-df = prepare_data(df)
-df = sidebar_filters(df)
-
-# --------------------------------------------------
+# ==========================================
 # KPI CARDS
-# --------------------------------------------------
+# ==========================================
 
 show_kpis(df)
 
 st.divider()
 
-# --------------------------------------------------
+# ==========================================
 # ROW 1
-# --------------------------------------------------
+# ==========================================
 
 col1, col2 = st.columns(2)
 
 with col1:
     st.plotly_chart(
         monthly_revenue_chart(df),
-        use_container_width=True,
+        use_container_width=True
     )
 
 with col2:
     st.plotly_chart(
         country_sales_chart(df),
-        use_container_width=True,
+        use_container_width=True
     )
 
-# --------------------------------------------------
+# ==========================================
 # ROW 2
-# --------------------------------------------------
+# ==========================================
 
 col3, col4 = st.columns(2)
 
 with col3:
     st.plotly_chart(
         top_products_chart(df),
-        use_container_width=True,
+        use_container_width=True
     )
 
 with col4:
     st.plotly_chart(
         quantity_chart(df),
-        use_container_width=True,
+        use_container_width=True
     )
 
-# --------------------------------------------------
+# ==========================================
 # ROW 3
-# --------------------------------------------------
+# ==========================================
 
 col5, col6 = st.columns(2)
 
 with col5:
     st.plotly_chart(
         country_donut(df),
-        use_container_width=True,
+        use_container_width=True
     )
 
 with col6:
     st.plotly_chart(
         country_map(df),
-        use_container_width=True,
+        use_container_width=True
     )
 
-# --------------------------------------------------
+# ==========================================
 # ROW 4
-# --------------------------------------------------
+# ==========================================
 
 st.plotly_chart(
     sales_heatmap(df),
-    use_container_width=True,
+    use_container_width=True
 )
 
 st.plotly_chart(
     daily_sales_chart(df),
-    use_container_width=True,
+    use_container_width=True
 )
+
 st.divider()
+
+# ==========================================
+# BUSINESS INSIGHTS
+# ==========================================
 
 show_insights(df)
